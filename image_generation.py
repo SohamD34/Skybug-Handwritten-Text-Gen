0341479fffac3805545338549770b7cb189ecc8b
SohamD34 <deshmukh.2@iitj.ac.in>
2024-01-14 14:37:51 +0530

    "Text to Image generation" added

---
+++ image_generation.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def create_image(sentences):
+    '''
+    Input - list of sentences(strings)
+    Output - Image 
+    Creates an image containing handwritten charaacaters from the text sentences.
+    '''
+    num_sentence = len(sentences)
+    max_len = max([len(sentence) for sentence in sentences])
+
+    symbols = ['.']
+
+    fig, axes = plt.subplots(num_sentence, max_len, figsize=(max_len,num_sentence*15))
+    plt.subplots_adjust(wspace=0, hspace=-0.1)
+
+    for j in range(num_sentence):
+
+        sentence = sentences[j]
+
+        chars = [char for char in sentence]
+        chars.extend([' ' for i in range(max_len - len(sentence))])
+
+        for i in range(len(chars)):
+
+            if(chars[i]>='Z'):
+                axes[j,i].imshow(Image.open('data/' + chars[i] + '_small.png'))
+
+            elif(chars[i]==' '):
+                axes[j,i].imshow(Image.open('data/space.png'))
+
+            elif(chars[i] in symbols):
+                axes[j,i].imshow(Image.open('data/space.png'))
+
+            elif(chars[i] == '?'):
+                axes[j,i].imshow(Image.open('data/question.jpg'))
+
+            else:
+                axes[j,i].imshow(Image.open('data/' + chars[i] + '.png'))
+
+            axes[j,i].axis('off')
+
+    plt.show()
+    plt.tight_layout()
+    return fig
+
+# text = ['Hello World', 'lorem ipsum','Jay Shree Ram','Jai Hanuman','Jai Bajrang Bali','Jai Shree Krishna']
+# fig = create_image(text)
+# plt.imshow(fig)
+# plt.show()
